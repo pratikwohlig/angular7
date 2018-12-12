7023e90268afb19f9adee8e022495a401ed065ab
pratik <pratik.shah@wohlig.com>
2018-12-12 15:49:56 +0530

    dynamic menu

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,9 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
+  navigation = [
+    {
+      name: "Home",
+      classis: "active",
+      anchor: "home",
+      subnav: [{
+          name: "Subnav1",
+          classis: "active",
+          anchor: "home"
+      }]
+    }, {
+      name: "Form",
+      classis: "active",
+      anchor: "form",
+      subnav: []
+    },
+    {
+        name: "Grid",
+        classis: "active",
+        anchor: "grid",
+        subnav: []
+    }
+  ];
   constructor() { }
-
+  
   ngOnInit() {
   }
 
